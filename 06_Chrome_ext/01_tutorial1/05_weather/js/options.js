ed12f69540fe814b4923c5e8f1d2c1671cc499c7
江泽源076634 <zeyuan.jzy@alibaba-inc.com>
2016-03-27 21:12:05 +0800

    06Chrome_ext/01_tutorial1/05_weather

---
+++ 06_Chrome_ext/01_tutorial1/05_weather/js/options.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jzy on 16/3/27.
+ */
+var city = localStorage.city || "beijing";
+document.getElementById("city").value = city;
+document.getElementById("save").onclick = function() {
+    localStorage.city = document.getElementById("city").value;
+    alert("保存成功!");
+}
